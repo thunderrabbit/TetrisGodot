9e7379d263292b3a8151b3fc84348fca9facec5f
Rob Nugen <rob@robnugen.com>
2017-12-14 21:00:53 -0300

    Mark unneeded files
    TODO: figure out how to remove them from Godot
    
    I tired to just wipe them via filesystem, but
    that caused Godot to get all confused.

---
+++ SubScenes/block.gd
@@ -0,0 +1,3 @@
+obviously do not need this, 
+
+but I am not sure how to wipe it from Godot
--- SubScenes/clickablock.gd
+++ SubScenes/clickablock.gd
@@ -1,10 +1,3 @@
-extends CollisionShape2D
+obviously do not need this, 
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+but I am not sure how to wipe it from Godot
